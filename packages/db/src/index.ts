e7849d04e687b2319fc180c4fb304fb30554691a
Hanfour.Huang <hanfourhuang@guoshipartners.com>
2026-04-17 17:04:18 +0800

    feat(db): add drizzle config and db client factory

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,1 +1,13 @@
-export const DB_PACKAGE_NAME = '@aide/db'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import pg from 'pg'
+import * as schema from './schema/index.js'
+
+export function createDb(databaseUrl: string) {
+  const pool = new pg.Pool({ connectionString: databaseUrl, max: 10 })
+  const db = drizzle(pool, { schema })
+  return { db, pool }
+}
+
+export type Database = ReturnType<typeof createDb>['db']
+export { schema }
+export * from './schema/index.js'
